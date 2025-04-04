fb2c9c1aff62b15f17521cf3cf9e452f75dc7848
Ilya Bykov <sovidze1dek@gmail.com>
2025-04-04 22:02:06 +0300

    Lesson #1

---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN = '7690983588:AAH9ZCPgA3ohYQWabC_l5OL-ci99P6mm7Pw'
+keys = {"рубль" : "RUB",
+        "доллар" : "USD",
+        "евро" : "EUR"}
---
+++ utils.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+class CryptoConverter:
+    def convert(self, quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f"Невозможно перевести одинаковые валюты {base}")
+        try:
+            quote_tick = keys[quote]
+        except KeyError:
+            raise ConvertionException(f"Не удалось перевести валюту{quote}!")
+        try:
+            base_tick = keys[base]
+        except KeyError:
+            raise  ConvertionException(f"Не удалось перевести валюту{base}")
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f"Не удалось обработать количество{amount}!")
+        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_tick}&tsyms={base_tick}")
+        total_base = json.loads(r.content)[keys[base]]
+        return total_base
+
+
