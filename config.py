88822d3b534b441ec77d4242034784c3ffb92480
IluaBukov <sovidze1dek@gmail.com>
2025-04-09 06:52:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-TOKEN = '7690983588:AAFWoHBie4O7wGz7JW8me5PaKA-9YkFphII'
+7690983588:AAFWoHBie4O7wGz7JW8me5PaKA-9YkFphII
+
 keys = {"рубль" : "RUB",
         "доллар" : "USD",
         "евро" : "EUR"}
